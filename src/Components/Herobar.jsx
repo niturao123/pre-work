e07db5fbe1a2e0aee1c9c4956f76826d220e0ffb
Savita Rao <savitarao702@gmail.com>
2025-04-07 15:10:57 +0530

    pushing herobar section

---
+++ src/Components/Herobar.jsx
@@ -0,0 +1,9 @@
+import "../Styles/Herobar.css"
+export const Herobar = ()=>{
+    return(
+        <div className="HerobarDiv">
+            <img src="herobar.png" alt="Herobar image" />
+            <p>Flexible everyday</p>
+        </div>
+    )
+}
